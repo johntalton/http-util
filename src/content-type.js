95968ad0d0f92e49fe65ccec38e54c3a75db9e54
johntalton <johntalton@gmail.com>
2026-01-10 17:56:54 -0500

    📝 extracted from tic

---
+++ src/content-type.js
@@ -0,0 +1,137 @@
+
+export const MIME_TYPE_JSON = 'application/json'
+export const MIME_TYPE_TEXT = 'text/plain'
+export const MIME_TYPE_EVENT_STREAM = 'text/event-stream'
+export const MIME_TYPE_XML = 'application/xml'
+export const MIME_TYPE_URL_FORM_DATA = 'application/x-www-form-urlencoded'
+export const MIME_TYPE_MULTIPART_FORM_DATA = 'multipart/form-data'
+export const MIME_TYPE_OCTET_STREAM = 'application/octet-stream'
+
+export const KNOWN_TYPES = [
+	'application', 'audio', 'image', 'message',
+	'multipart','text', 'video'
+]
+
+export const TYPE_X_TOKEN_PREFIX = 'X-'
+
+export const SPECIAL_CHARS = [
+	// special
+	'(', ')', '<', '>',
+	'@', ',', ';', ':',
+	'\\', '"', '/', '[',
+	']', '?', '.', '=',
+	// space
+	' ', '\u000B', '\u000C',
+	// control
+	'\n', '\r', '\t'
+]
+
+export function isWhitespace(c){ return /\s/.test(c) }
+
+/**
+ * @param {string|undefined} value
+ */
+export function hasSpecialChar(value) {
+  if(value === undefined) { return false }
+  for(const special of SPECIAL_CHARS) {
+    if(value.includes(special)) { return true}
+  }
+
+  return false
+}
+
+/**
+ * @typedef {Object} ContentType
+ * @property {string} mimetype
+ * @property {string} mimetypeRaw
+ * @property {string} type
+ * @property {string} subtype
+ * @property {string} [charset]
+ * @property {Map<string, string>} parameters
+ */
+
+export const SEPARATOR = {
+	SUBTYPE: '/',
+	PARAMETER: ';',
+	KVP: '='
+}
+
+export const CHARSET_UTF8 = 'utf8'
+export const CHARSET = 'charset'
+export const PARAMETER_CHARSET_UTF8 = `${CHARSET}${SEPARATOR.KVP}${CHARSET_UTF8}`
+export const CONTENT_TYPE_JSON = `${MIME_TYPE_JSON}${SEPARATOR.PARAMETER}${PARAMETER_CHARSET_UTF8}`
+export const CONTENT_TYPE_TEXT = `${MIME_TYPE_TEXT}${SEPARATOR.PARAMETER}${PARAMETER_CHARSET_UTF8}`
+
+/** @type {ContentType} */
+export const WELL_KNOWN_JSON = {
+	mimetype: 'application/json',
+	mimetypeRaw: 'application/json',
+	type: 'application',
+	subtype: 'json',
+	charset: 'utf8',
+	parameters: new Map()
+}
+
+export const WELL_KNOWN = new Map([
+	[ 'application/json', WELL_KNOWN_JSON ],
+	[ 'application/json;charset=utf8', WELL_KNOWN_JSON ]
+])
+
+
+/**
+ * @param {string|undefined} contentTypeHeader
+ * @returns {ContentType|undefined}
+ */
+export function parseContentType(contentTypeHeader) {
+	if(contentTypeHeader === undefined) { return undefined }
+	if(contentTypeHeader === null) { return undefined }
+
+	const wellKnown = WELL_KNOWN.get(contentTypeHeader)
+	if(wellKnown !== undefined) { return wellKnown }
+
+	const [ mimetypeRaw, ...parameterSet ] = contentTypeHeader.split(SEPARATOR.PARAMETER)
+	if(mimetypeRaw === undefined) { return undefined }
+	if(mimetypeRaw === '') { return undefined }
+
+	const [ typeRaw, subtypeRaw ] = mimetypeRaw
+		.split(SEPARATOR.SUBTYPE)
+		.map(t => t.toLowerCase())
+
+	if(typeRaw === undefined) { return undefined }
+	if(typeRaw === '') { return undefined }
+	if(hasSpecialChar(typeRaw)) { return undefined }
+	if(subtypeRaw === undefined) { return undefined }
+	if(subtypeRaw === '') { return undefined }
+	if(hasSpecialChar(subtypeRaw)) { return undefined }
+
+	const type = typeRaw.trim()
+	const subtype = subtypeRaw.trim()
+
+	const parameters = new Map()
+
+	parameterSet
+			.forEach(parameter => {
+				const [ key, value ] = parameter.split(SEPARATOR.KVP)
+				if(key === undefined || key === '') { return }
+				if(value === undefined || value === '') { return }
+				if(hasSpecialChar(key)) { return }
+
+				const actualKey = key?.trim().toLowerCase()
+
+				const quoted = (value.charAt(0) === '"' && value.charAt(value.length - 1) === '"')
+				const actualValue = quoted ? value.substring(1, value.length - 1) : value
+
+				if(!parameters.has(actualKey)) {
+					parameters.set(actualKey, actualValue)
+				}
+			})
+
+	const charset = parameters.get(CHARSET)
+
+	return {
+		mimetype: `${type}${SEPARATOR.SUBTYPE}${subtype}`,
+		mimetypeRaw, type, subtype,
+		charset,
+		parameters
+	}
+}
---
+++ src/handle-stream-util.js
@@ -0,0 +1,261 @@
+import http2 from 'node:http2'
+import { brotliCompressSync, deflateSync, gzipSync, zstdCompressSync } from 'node:zlib'
+
+import {
+	SSE_MIME,
+	SSE_INACTIVE_STATUS_CODE,
+	SSE_BOM,
+	ENDING,
+} from '@johntalton/sse-util'
+
+import { CHARSET_UTF8, CONTENT_TYPE_JSON, CONTENT_TYPE_TEXT } from './content-type.js'
+import { ServerTiming, HTTP_HEADER_SERVER_TIMING, HTTP_HEADER_TIMING_ALLOW_ORIGIN } from './server-timing.js'
+import { HTTP_HEADER_RATE_LIMIT, HTTP_HEADER_RATE_LIMIT_POLICY, RateLimit, RateLimitPolicy } from './rate-limit.js'
+
+const {
+	HTTP2_HEADER_STATUS,
+	HTTP2_HEADER_CONTENT_TYPE,
+	HTTP2_HEADER_CONTENT_ENCODING,
+	HTTP2_HEADER_VARY,
+	HTTP2_HEADER_ACCESS_CONTROL_ALLOW_ORIGIN,
+	HTTP2_HEADER_ACCESS_CONTROL_ALLOW_METHODS,
+	HTTP2_HEADER_ACCESS_CONTROL_ALLOW_HEADERS,
+	HTTP2_HEADER_ACCESS_CONTROL_ALLOW_CREDENTIALS,
+	HTTP2_HEADER_SERVER,
+	HTTP2_HEADER_RETRY_AFTER,
+	HTTP2_HEADER_CACHE_CONTROL
+} = http2.constants
+
+const {
+	HTTP_STATUS_OK,
+	HTTP_STATUS_NOT_FOUND,
+	HTTP_STATUS_UNAUTHORIZED,
+	HTTP_STATUS_NO_CONTENT,
+	HTTP_STATUS_INTERNAL_SERVER_ERROR,
+	HTTP_STATUS_TOO_MANY_REQUESTS
+} = http2.constants
+
+export const HTTP_HEADER_ORIGIN = 'origin'
+export const HTTP_HEADER_USER_AGENT = 'user-agent'
+export const HTTP_HEADER_FORWARDED = 'forwarded'
+export const HTTP_HEADER_SEC_CH_UA = 'sec-ch-ua'
+export const HTTP_HEADER_SEC_CH_PLATFORM = 'sec-ch-ua-platform'
+export const HTTP_HEADER_SEC_CH_MOBILE = 'sec-ch-ua-mobile'
+export const HTTP_HEADER_SEC_FETCH_SITE = 'sec-fetch-site'
+export const HTTP_HEADER_SEC_FETCH_MODE = 'sec-fetch-mode'
+export const HTTP_HEADER_SEC_FETCH_DEST = 'sec-fetch-dest'
+
+export const DEFAULT_METHODS = [ 'HEAD', 'GET', 'POST', 'PATCH', 'DELETE' ]
+
+export const HTTP2_HEADER_ACCESS_CONTROL_MAX_AGE = 'access-control-max-age'
+export const PREFLIGHT_AGE_SECONDS = '500'
+
+/**
+ * @import { ServerHttp2Stream } from 'node:http2'
+ */
+
+/**
+ * @import { TimingsInfo } from './server-timing.js'
+ */
+
+/**
+ * @typedef {Object} Metadata
+ * @property {Array<TimingsInfo>} performance
+ * @property {string|undefined} servername
+ * @property {string|undefined} origin
+ */
+
+/**
+ * @typedef {Object} SSEOptions
+ * @property {boolean} [active]
+ * @property {boolean} [bom]
+ */
+
+/**
+ * @param {ServerHttp2Stream} stream
+ * @param {string} message
+ * @param {Metadata} meta
+ */
+export function sendError(stream, message, meta) {
+	console.error('500', message)
+
+	if(stream === undefined) { return }
+	if(stream.closed) { return }
+
+	if(!stream.headersSent) {
+		stream.respond({
+			[HTTP2_HEADER_ACCESS_CONTROL_ALLOW_ORIGIN]: meta.origin,
+			[HTTP2_HEADER_STATUS]: HTTP_STATUS_INTERNAL_SERVER_ERROR,
+			[HTTP2_HEADER_CONTENT_TYPE]: CONTENT_TYPE_TEXT,
+			[HTTP2_HEADER_SERVER]: meta.servername
+		})
+	}
+
+	// protect against HEAD calls
+	if(stream.writable) {
+		if(message !== undefined) { stream.write(message) }
+	}
+
+	stream.end()
+	if(!stream.closed) { stream.close() }
+}
+
+/**
+ * @param {ServerHttp2Stream} stream
+ * @param {string|undefined} allowedOrigin
+ * @param {Array<string>} methods
+ * @param {Metadata} meta
+ */
+export function sendPreflight(stream, allowedOrigin, methods, meta) {
+	stream.respond({
+		[HTTP2_HEADER_ACCESS_CONTROL_ALLOW_ORIGIN]: allowedOrigin,
+		[HTTP2_HEADER_ACCESS_CONTROL_ALLOW_METHODS]: methods.join(','),
+		[HTTP2_HEADER_ACCESS_CONTROL_ALLOW_HEADERS]: ['Authorization', HTTP2_HEADER_CONTENT_TYPE].join(','),
+		[HTTP2_HEADER_ACCESS_CONTROL_MAX_AGE]: PREFLIGHT_AGE_SECONDS,
+		[HTTP2_HEADER_SERVER]: meta.servername
+	})
+	stream.end()
+}
+
+/**
+ * @param {ServerHttp2Stream} stream
+ * @param {Metadata} meta
+ */
+export function sendUnauthorized(stream, meta) {
+	console.log('Unauthorized')
+
+	stream.respond({
+		[HTTP2_HEADER_ACCESS_CONTROL_ALLOW_ORIGIN]: meta.origin,
+		[HTTP2_HEADER_STATUS]: HTTP_STATUS_UNAUTHORIZED,
+		[HTTP2_HEADER_SERVER]: meta.servername
+	})
+	stream.end()
+}
+
+/**
+ * @param {ServerHttp2Stream} stream
+ * @param {string} message
+ * @param {Metadata} meta
+ */
+export function sendNotFound(stream, message, meta) {
+	console.log('404', message)
+	stream.respond({
+		[HTTP2_HEADER_ACCESS_CONTROL_ALLOW_ORIGIN]: meta.origin,
+		[HTTP2_HEADER_STATUS]: HTTP_STATUS_NOT_FOUND,
+		[HTTP2_HEADER_CONTENT_TYPE]: CONTENT_TYPE_TEXT,
+		[HTTP2_HEADER_SERVER]: meta.servername
+	})
+
+	if(message !== undefined) { stream.write(message) }
+	stream.end()
+}
+
+/**
+ * @param {ServerHttp2Stream} stream
+ * @param {*} limitInfo
+ * @param {Array<any>} policies
+ * @param {Metadata} meta
+ */
+export function sendTooManyRequests(stream, limitInfo, policies, meta) {
+	stream.respond({
+		[HTTP2_HEADER_ACCESS_CONTROL_ALLOW_ORIGIN]: meta.origin,
+		[HTTP2_HEADER_STATUS]: HTTP_STATUS_TOO_MANY_REQUESTS,
+		[HTTP2_HEADER_SERVER]: meta.servername,
+
+		[HTTP2_HEADER_RETRY_AFTER]: limitInfo.retryAfterS,
+		[HTTP_HEADER_RATE_LIMIT]: RateLimit.from(limitInfo),
+		[HTTP_HEADER_RATE_LIMIT_POLICY]: RateLimitPolicy.from(...policies)
+	})
+
+	stream.write(`Retry After ${limitInfo.retryAfterS} Seconds`)
+	stream.end()
+}
+
+/**
+ * @typedef { (data: string, charset: BufferEncoding) => Buffer } EncoderFun
+ */
+
+/** @type {Map<string, EncoderFun>} */
+export const ENCODER_MAP = new Map([
+	[ 'br', (data, charset) => brotliCompressSync(Buffer.from(data, charset)) ],
+	[ 'gzip', (data, charset) => gzipSync(Buffer.from(data, charset)) ],
+	[ 'deflate', (data, charset) => deflateSync(Buffer.from(data, charset)) ],
+	[ 'zstd', (data, charset) => zstdCompressSync(Buffer.from(data, charset)) ]
+])
+
+/**
+ * @param {ServerHttp2Stream} stream
+ * @param {Object} obj
+ * @param {string|undefined} encoding
+ * @param {string|undefined} allowedOrigin
+ * @param {Metadata} meta
+ */
+export function sendJSON_Encoded(stream, obj, encoding, allowedOrigin, meta) {
+	if(stream.closed) { return }
+
+	const json = JSON.stringify(obj)
+
+	const useIdentity = encoding === 'identity'
+	const encoder = encoding !== undefined ? ENCODER_MAP.get(encoding) : undefined
+	const hasEncoder = encoder !== undefined
+	const actualEncoding = hasEncoder ? encoding : undefined
+
+	const encodeStart = performance.now()
+	const encodedData = hasEncoder && !useIdentity ? encoder(json, CHARSET_UTF8) : json
+	const encodeEnd = performance.now()
+
+	meta.performance.push(
+		{ name: 'encode', duration: encodeEnd - encodeStart }
+	)
+
+	stream.respond({
+		[HTTP2_HEADER_ACCESS_CONTROL_ALLOW_ORIGIN]: allowedOrigin,
+		[HTTP2_HEADER_CONTENT_TYPE]: CONTENT_TYPE_JSON,
+		[HTTP2_HEADER_CONTENT_ENCODING]: actualEncoding,
+		[HTTP2_HEADER_VARY]: 'Accept, Accept-Encoding',
+		[HTTP2_HEADER_CACHE_CONTROL]: 'private',
+		[HTTP2_HEADER_STATUS]: HTTP_STATUS_OK,
+		[HTTP2_HEADER_SERVER]: meta.servername,
+		[HTTP_HEADER_TIMING_ALLOW_ORIGIN]: allowedOrigin,
+		[HTTP_HEADER_SERVER_TIMING]: ServerTiming.encode(meta.performance)
+	})
+
+	// stream.write(encodedData)
+	stream.end(encodedData)
+}
+
+/**
+ * @param {ServerHttp2Stream} stream
+ * @param {string|undefined} allowedOrigin
+ * @param {SSEOptions & Metadata} meta
+ */
+export function sendSSE(stream, allowedOrigin, meta) {
+	// stream.setTimeout(0)
+	// stream.session?.setTimeout(0)
+	// stream.session?.socket.setTimeout(0)
+	// stream.session.socket.setNoDelay(true)
+	// stream.session.socket.setKeepAlive(true)
+
+	// stream.on('close', () => console.log('SSE stream closed'))
+	// stream.on('aborted', () => console.log('SSE stream aborted'))
+
+	const activeStream = meta.active ?? true
+	const sendBOM = meta.bom ?? true
+
+	stream.respond({
+		[HTTP2_HEADER_ACCESS_CONTROL_ALLOW_ORIGIN]: allowedOrigin,
+		[HTTP2_HEADER_CONTENT_TYPE]: SSE_MIME,
+		[HTTP2_HEADER_STATUS]: activeStream ? HTTP_STATUS_OK : HTTP_STATUS_NO_CONTENT, // SSE_INACTIVE_STATUS_CODE
+		// [HTTP2_HEADER_ACCESS_CONTROL_ALLOW_CREDENTIALS]: 'true'
+		[HTTP2_HEADER_SERVER]: meta.servername
+	 })
+
+	 if(!activeStream) {
+		stream.end()
+		return
+	 }
+
+	if(sendBOM) {
+		stream.write(SSE_BOM + ENDING.CRLF)
+	}
+}
